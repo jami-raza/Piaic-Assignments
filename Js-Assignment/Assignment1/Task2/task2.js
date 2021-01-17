b80cadb241e318052560802372d1f536a4899769
jami raza <muhammadjmai45@gmail.com>
2021-01-17 20:23:05 +0500

    task2 added

---
+++ Js-Assignment/Assignment1/Task2/task2.js
@@ -0,0 +1,9 @@
+var name = "John Doe";
+var age = 15;
+var course = "Certfied Mobile Application Development"
+
+
+
+alert("Name \r\n" + " " + name);
+alert("Age \r\n" + " " + age);
+alert("Cousrse \r\n" + " " + course);
