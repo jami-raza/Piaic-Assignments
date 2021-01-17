44729b22d0be72bcd0b4a7987e6e9181cf368337
jami raza <muhammadjmai45@gmail.com>
2021-01-17 21:00:17 +0500

    task 6 added

---
+++ Js-Assignment/Assignment1/Task6/task6.js
@@ -0,0 +1,4 @@
+var birth_year = 1999;
+
+document.getElementById('head').innerHTML = "My birth year is" + " " + birth_year; 
+document.getElementById("para").innerHTML = "My declared variable type is" + " " + typeof birth_year; 
