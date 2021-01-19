f4ba901be513535d89e0c1f664b8725f37d4af43
jami raza <muhammadjmai45@gmail.com>
2021-01-19 11:26:49 +0500

    Assignment 2 task 1 added

---
+++ Js-Assignment/Assignment2_PIAIC81730/Task1/task1.js
@@ -0,0 +1,8 @@
+var city = prompt("In which City do you live ?")
+
+if (city.toUpperCase('karachi')){
+     var a = document.createElement("h1");
+     a.innerHTML = "City of Lights &#128081;"
+     document.body.appendChild(a)
+     a.style.textAlign = "center"
+}
